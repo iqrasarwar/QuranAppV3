7fc0b8c8e00cc5b855c9e8108d9e73fc04dcaeac
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-08-19 02:36:48 +0500

    about fragment

--- app/src/main/java/com/example/quranappv3/SplashActivity.java
+++ app/src/main/java/com/example/quranappv3/SplashActivity.java
@@ -2,13 +2,28 @@ package com.example.quranappv3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        private static int SPLASH_SCREEN_TIME_OUT=2000;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            getWindow().setFlags(WindowManager
+                            .LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            setContentView(R.layout.activity_splash);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent i=new Intent(SplashActivity.this,
+                            NavigationDrawerActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }, SPLASH_SCREEN_TIME_OUT);
     }
 }
--- app/src/main/java/com/example/quranappv3/ui/About/AboutFragment.java
+++ app/src/main/java/com/example/quranappv3/ui/About/AboutFragment.java
@@ -1,5 +1,8 @@
 package com.example.quranappv3.ui.About;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +10,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.quranappv3.R;
+import com.example.quranappv3.databinding.FragmentAboutBinding;
+import com.example.quranappv3.databinding.FragmentSettingsBinding;
+
 public class AboutFragment extends Fragment {
 
+    private FragmentAboutBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,40 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        binding = FragmentAboutBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        ImageView call = binding.imageView3;
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:+9233244670385");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        ImageView email = binding.imageView7;
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("*/*");
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, "bsef19m012@pucit.edu.pk");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Quran App Contact Mail");
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+                else {
+                    Context context = getContext();
+                    CharSequence text = "No app to handle email intent!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+            }
+        });
+        return root;
     }
 }
